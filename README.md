eccffff7655a3b6b5ede2da1d8ebe28aaa6f5939
Abhay Vardhan <abhay@blip.me>
2014-01-17 08:42:26 -0800

    Updated readme, changed API

--- README.md
+++ README.md
@@ -8,4 +8,32 @@ food truck permits have been issued by the city of San Francisco.
 
 The backend is to be used with abhayv/foodtrucks-frontend which uses Google Maps to display the location of
 food trucks.
- 
+
+The API is as follows:
+
+HTTP GET /search
+    Search for food truck locations given a query and a rectangular area defined by coordinates of the south west
+    and north east corners:
+    query: String to search for
+    southWestLat: Latitude of the south west corner
+    southWestLng: Longitude of the south west corner
+    northEastLat: Latitude of the north east corner
+    northEastLng: Longitude of the north east corner
+    jsoncallback: Name of the JSONP callback function wrapper
+
+    :return: JSONP encoded result in the following format:
+    jsoncallback({'success': true, 'data': [
+    <food_truck_data_1>,
+    <food_truck_data_2>
+    ]})
+
+    Where food_truck_data is a JSON object with info fields described in http://data.sfgov.org/resource/rqzj-sfat.json
+
+HTTP GET /build-index
+    Fetches the data again from the source and rebuild the index
+
+
+Testing
+-------
+
+Open tests/SpecRunner.html in a browser or run using phantomjs
--- api_server.py
+++ api_server.py
@@ -15,11 +15,30 @@ import search_index
 
 @app.route('/search')
 def search():
-    logging.info("Incoming %s" % request.args)
+    """
+    Search for food truck locations given a query and a rectangular area defined by coordinates of the south west
+    and north east corners:
+    query: String to search for
+    southWestLat: Latitude of the south west corner
+    southWestLng: Longitude of the south west corner
+    northEastLat: Latitude of the north east corner
+    northEastLng: Longitude of the north east corner
+    jsoncallback: Name of the JSONP callback function wrapper
+
+    :return: JSONP encoded result in the following format:
+    jsoncallback({'success': true, 'data': [
+    <food_truck_data_1>,
+    <food_truck_data_2>
+    ]})
+
+    Where food_truck_data is a JSON object with info fields described in http://data.sfgov.org/resource/rqzj-sfat.json
+    """
+    logging.debug("Incoming %s" % request.args)
     callback_name = request.args.get('jsoncallback')
-    result = search_index.search(request.args.get('query'),
+    results = search_index.search(request.args.get('query'),
                                  float(request.args.get('southWestLat')),
                                  float(request.args.get('southWestLng')),
                                  float(request.args.get('northEastLat')),
                                  float(request.args.get('northEastLng')))
-    return Response('%s(%s)' % (callback_name, json.dumps(result)), content_type='application/javascript; charset=utf-8')
+    return Response('%s(%s)' % (callback_name, json.dumps({'success': True, 'data': results})),
+                    content_type='application/javascript; charset=utf-8')
